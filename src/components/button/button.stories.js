fc4e83c7fd924c76eda1c3ccdc4efc6b05336b6c
danifosy <mail@daniha.de>
2021-07-22 13:38:01 +0200

    Fix naming

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,9 +1,9 @@
 import "./button.css";
-import buttonLogo from "./button.html";
+import actionButton from "./button.html";
 
 export default {
   title: "Components/Button",
   parameters: { layout: "fullscreen" },
 };
 
-export const withLogo = () => buttonLogo;
+export const withLogo = () => actionButton;
