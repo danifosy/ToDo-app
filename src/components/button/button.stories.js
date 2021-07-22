75700d748a641413b1e710c01af273dc91295a3d
danifosy <mail@daniha.de>
2021-07-22 11:56:05 +0200

    Add button

---
+++ src/components/button/button.stories.js
@@ -0,0 +1,9 @@
+import "./button.css";
+import buttonLogo from "./button.html";
+
+export default {
+  title: "Components/Button",
+  parameters: { layout: "fullscreen" },
+};
+
+export const withLogo = () => buttonLogo;
