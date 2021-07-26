24d85a8449a59b771c87dfb1871d3cf1f4e11b75
danifosy <mail@daniha.de>
2021-07-26 15:46:17 +0200

    Create files

---
+++ src/components/iconButton/iconButton.stories.js
@@ -0,0 +1,9 @@
+import "./iconButton.css";
+import iconButton from "./iconButton.html";
+
+export default {
+  title: "Components/Form",
+  parameters: { layout: "fullscreen" },
+};
+
+export const button = () => iconButton;
