bf5cd6e29179542b18c85f02fc3d4d30d1f635c2
danifosy <mail@daniha.de>
2021-07-26 16:09:42 +0200

    Add Bell, change .stories.js

--- src/components/iconButton/iconButton.stories.js
+++ src/components/iconButton/iconButton.stories.js
@@ -1,9 +1,11 @@
 import "./iconButton.css";
-import iconButton from "./iconButton-menu.html";
+import iconButtonMenu from "./iconButton-menu.html";
+import iconButtonBell from "./iconButton-bell.html";
 
 export default {
   title: "Components/IconButton",
   parameters: { layout: "fullscreen" },
 };
 
-export const button = () => iconButton;
+export const Menu = () => iconButtonMenu;
+export const Bell = () => iconButtonBell;
