61ef6f1c3286d512aef460a07af153c7ad0073cd
danifosy <mail@daniha.de>
2021-07-28 14:34:24 +0200

    Add const in function

--- newTask.js
+++ newTask.js
@@ -1,12 +1,10 @@
-const { formatDiagnostic } = require("typescript");
-
 const addTaskForm = document.querySelector(".addTaskForm");
 const taskFormInput = document.querySelector(".addTaskForm__textInput");
-const taskFormRadio = document.querySelector(
-  ".addTaskForm__radio:checked + .addTaskForm__radioLabel"
-);
-const taskFormSubmit = document.querySelector(".addTaskForm__submit");
+const taskFormRadio = document.querySelector(".addTaskForm__radio:checked");
 
-form.onsubmit = function (event) {
+addTaskForm.onsubmit = function (event) {
   event.preventDefault();
+  const taskFormValue = taskFormInput.value;
+  const taskFormDate = taskFormRadio.value;
+  console.log(taskFormValue, taskFormDate);
 };
