79fd9556e180bfff2e8c92861cec4a58fa43fa72
danifosy <mail@daniha.de>
2021-07-28 14:17:08 +0200

    Add function to .js

--- newTask.js
+++ newTask.js
@@ -1,5 +1,12 @@
+const { formatDiagnostic } = require("typescript");
+
+const addTaskForm = document.querySelector(".addTaskForm");
 const taskFormInput = document.querySelector(".addTaskForm__textInput");
 const taskFormRadio = document.querySelector(
   ".addTaskForm__radio:checked + .addTaskForm__radioLabel"
 );
 const taskFormSubmit = document.querySelector(".addTaskForm__submit");
+
+form.onsubmit = function (event) {
+  event.preventDefault();
+};
