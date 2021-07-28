749e721c21617c298a208938690c0ba50e8feeff
danifosy <mail@daniha.de>
2021-07-28 14:11:01 +0200

    Add consts for input, radio and submit

---
+++ newTask.js
@@ -0,0 +1,5 @@
+const taskFormInput = document.querySelector(".addTaskForm__textInput");
+const taskFormRadio = document.querySelector(
+  ".addTaskForm__radio:checked + .addTaskForm__radioLabel"
+);
+const taskFormSubmit = document.querySelector(".addTaskForm__submit");
