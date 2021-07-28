358c7cd67227cf754c7e0ec094e65cc00a5ee7f6
danifosy <mail@daniha.de>
2021-07-28 14:38:12 +0200

    Fix syntax error

--- newTask.js
+++ newTask.js
@@ -1,9 +1,9 @@
 const addTaskForm = document.querySelector(".addTaskForm");
 const taskFormInput = document.querySelector(".addTaskForm__textInput");
-const taskFormRadio = document.querySelector(".addTaskForm__radio:checked");
 
 addTaskForm.onsubmit = function (event) {
   event.preventDefault();
+  const taskFormRadio = document.querySelector(".addTaskForm__radio:checked");
   const taskFormValue = taskFormInput.value;
   const taskFormDate = taskFormRadio.value;
   console.log(taskFormValue, taskFormDate);
